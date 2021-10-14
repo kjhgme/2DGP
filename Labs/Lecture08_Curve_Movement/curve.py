b2989583c28220e2c4ae46a6303235c00de782e1
kjhgme <kjhgme@naver.com>
2021-10-14 10:44:14 +0900

    Update curve.py

--- Labs/Lecture08_Curve_Movement/curve.py
+++ Labs/Lecture08_Curve_Movement/curve.py
@@ -55,6 +55,20 @@ def draw_point(p):
 
 def draw_curve_3_points(p1, p2, p3):
     # fill here
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
+
+    x1, y1 = p1; x2, y2 = p2; x3, y3 = p3
+
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * x1 + (-4 * t ** 2 + 4 * t) * x2 + (2 * t ** 2 - t) * x3
+        y = (2 * t ** 2 - 3 * t + 1) * y1 + (-4 * t ** 2 + 4 * t) * y2 + (2 * t ** 2 - t) * y3
+        draw_point((x, y))
+
+    draw_point(p3)
+
     pass
 
 
@@ -88,14 +102,63 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
+def draw_line(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
 
+    draw_big_point(p1)
+    draw_big_point(p2)
+
+    for i in range(0, 100+1, 2):
+        t = i / 100
+        x = (1-t)*x1 + t*x2
+        y = (1-t)*y1 + t*y2
+        draw_point((x, y))
+
+    draw_point(p2)
+
+def blend_lines(p1, p2, p3, p4):
+    x1, y1 = p1; x2, y2 = p2; x3, y3 = p3; x4, y4 = p4
+
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
+    draw_big_point(p4)
+
+    #draw 2 lines
+    #draw_line(p2, p3)
+    #draw_line(p4, p1)
+
+    for i in range(0, 100, 2):
+        t = i / 100
+        lx = (1-t) * x2 + t * x3
+        ly = (1-t) * y2 + t * y3
+
+        rx = (1-t) * x4 + t * x1
+        ry = (1-t) * y4 + t * y1
+
+        draw_point((lx, ly))
+        draw_point((rx, ry))
+
+        x = (1-t)*lx + t * rx
+        y = (1-t)*ly + t * ry
+
+        draw_point((x, y))
 
 
 
 prepare_turtle_canvas()
 
+p1 = x1, y1 = random.randint(100, 300), random.randint(100, 300)
+p2 = x2, y2 = random.randint(-300, -200), random.randint(100, 300)
+p3 = x3, y3 = random.randint(-300, -200), random.randint(-300, -100)
+p4 = x4, y4 = random.randint(200, 300), random.randint(-300, -100)
 
+#blend_lines(p1, p2, p3, p4)
 
+#draw_curve_3_points((-350,-100), (-50, 200), (150, -100))
+#draw_curve_3_points((-50,200), (150, -100), (350, 300))
 
+draw_curve_4_points(p1, p3, p4, p2)
 
 turtle.done()
